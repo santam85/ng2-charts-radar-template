66250b10e588f4edd25520190ff91d373338dab7
Marco Santarelli <marco@backbase.com>
2021-08-09 13:12:42 +0200

    Updated to rc.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartDataSets, ChartType, RadialChartOptions } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Component } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
-  // Radar
-  public radarChartOptions: RadialChartOptions = {
-    responsive: true,
-  };
-  public radarChartLabels: Label[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+export class AppComponent {
+  title = 'ng2-charts-demo';
 
-  public radarChartData: ChartDataSets[] = [
+  public radarChartOptions: ChartConfiguration<'radar'>['options'] = {
+    responsive: false,
+  };
+  public radarChartLabels: string[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
+
+  public radarChartDatasets: ChartConfiguration<'radar'>['data']['datasets'] = [
     { data: [65, 59, 90, 81, 56, 55, 40], label: 'Series A' },
     { data: [28, 48, 40, 19, 96, 27, 100], label: 'Series B' }
   ];
-  public radarChartType: ChartType = 'radar';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
+
 }
